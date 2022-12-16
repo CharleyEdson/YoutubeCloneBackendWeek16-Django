8f7bb99c5bd8f07ca855a031b2f9cddfc4f4a0b4
Charley <charley1135@gmail.com>
2022-12-16 09:21:43 -0800

    feat: Added in Comments

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,15 @@
+
+
+const SearchBar = (props) => {
+  return (
+    <div>
+      <div> Search For Videos</div>
+      <div>
+        <input></input>
+        <button><link to ="/searchresults"></link></button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
